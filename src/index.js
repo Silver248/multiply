dc91f09318b72c33b61b519f5120ec0c04b28ecd
Silver248 <wmanzhela@gmail.com>
2019-10-19 02:13:06 +0300

    feat; add solution to Multiply task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 module.exports = function multiply(first, second) {
-  let m = first*second;
-  m.length = 100;
-  let n = String(m);
-  return(n);
+  
+
+  n = BigInt(first);
+  m = BigInt(second);
+  return String(BigInt(n*m));
+  
+
+  /*let f=first.split("");
+  let s=second.split("");  
+  
+  for (let i=length.f; i>=0; i--){
+    for (let j=length.s; j>=0; j--){
+      let s[j]*f[i]
+  }*/
 }
